30bffb595240b0d4ae4c79204736f72caface1b2
Your Name <juanaya@unicauca.edu.co>
2021-10-04 01:04:56 -0500

    AssignTask

--- src/Components/forProfessor/Profile/Assign-Task/Assign-Task.js
+++ src/Components/forProfessor/Profile/Assign-Task/Assign-Task.js
@@ -1,34 +1,86 @@
 import React from "react";
+import {useHistory} from "react-router-dom";
+import "./Assign-Task.css";
+
 
 
 export default function AssignTask() {
     
-  return (
-        <div className="contGrande">
+    return (
+        <div className="contAssign">
+            <div className="contAssign1">
+                    <div className="col element title">
+                    Assign Task To Students
+                    </div>
+            </div>
             
-            <div className="cont1">
-                <div className="col element title">
-                    Fulfill Self-Assesment of house
-                </div>   
-                <div>
-                </div> 
-                
-                
-            </div>
-                
-            <div className="cont2">
-                        
-                <div className="col element title">
-                    Course Development
+            <div className="contAssign2">
+                <div className="firstC">
+                    <div className="Course">
+                    Course
+                    </div>
+                    <div className="Design">
+                    Design of chemical and Biochemical processes
+                    </div>
                 </div>
+                <div className="firstC">
+                    <div className="Course">
+                    Student Outcome
+                    </div>
+                    <div className="Design">
+                    3.Ability to communicate effectively with a range of audiences
+                    </div>
+                </div>
+                <div className="firstC">
+                    <div className="Course">
+                    Task name
+                    </div>
+                    <div className="Design">
+                    Ensayo
+                    </div>
+                </div>
+                <div className="firstC">
+                    <div className="Course">
+                    Deadline
+                    </div>
+                    <div className="Design">
+                    Date & hour
+                    </div>
+                </div>
+                <div className="firstC">
+                    <div className="Course">
+                    Task description
+                    </div>
+                    <div className="Design">
+                    Dear students...
+                    </div>
+                </div>
+                <div className="firstC">
+                    <div className="Course">
+                    Required file
+                    </div>
+                    <div className="Design">
+                    Pdf
+                    </div>
+                </div>
+                    
                 
-                        
             </div>
-        </div>
             
                 
             
     
+        </div>
+
         
     );
+
 }
+
+/*
+Fulfill Self-Assesment of house
+Course Development
+
+
+
+*/
