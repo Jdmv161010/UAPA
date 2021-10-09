e62de57821b8bf984bb59fb0eca43308f54d523a
Your Name <juanaya@unicauca.edu.co>
2021-10-08 22:22:11 -0500

    Loginupdate

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -2,21 +2,13 @@ import * as actionTypes from "../types/types";
 
 const initialState = {
   responseLogin: {
-    access: "",
-    refresh: "",
-    username: "",
-    role: "",
+    
   },
 };
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.START_AUTH_LOGIN:
-      return {
-        ...state,
-        //Do something
-      };
-      break;
+   
     case actionTypes.AUTH_LOGIN:
       return {
         ...state,
